7fb0d413994541528d110c936d3be8558efa4b15
Sangram Kapre <sangram@sangram-Ubuntu12.04>
2013-10-28 18:37:00 +0530

    get picture URL along with the profile from linkedin

--- app/lib/my_linkedin.py
+++ app/lib/my_linkedin.py
@@ -47,7 +47,7 @@ class MyLinkedIn(Config):
 		self.url_profile = self.get_cfg("LinkedIn", "url_profile")
 		self.create_token(oauth_key, oauth_secret)
 		self.prepare_client()
-		self.fetch_profile_query = profile_id + ":(id,first-name,last-name,distance,relation-to-viewer,public-profile-url)?format=json"
+		self.fetch_profile_query = profile_id + ":(id,first-name,last-name,distance,relation-to-viewer,public-profile-url,picture-url::(original))?format=json"
 		resp, content = self.client.request(self.url_profile + self.fetch_profile_query)
 		print "get_profile_using_id()"
 		return content
