afad6ba145921684e3942b995cb06d0f831d7227
Sangram Kapre <sangram@sangram-Ubuntu12.04>
2013-10-28 18:21:02 +0530

    testcases for Python code

--- app/app_test.py
+++ app/app_test.py
@@ -17,26 +17,51 @@ class LeadsTestCase(unittest.TestCase):
 		#os.close(self.db_fd)
 		#os.unlink(leadsApp.app.config['DATABASE'])
 
+	# homepage
 	def test_homepage(self):
 		response = self.app.get('/')
 		#print response
 		self.assertEqual(response.status_code, 200)
 		
+	# searching
 	def test_search(self):
 		response = self.app.get('/search?fname=sangram&lname=kapre&cname=.')
 		#print response.data
 		self.assertEqual(response.status_code, 200)
 
+	def test_search_fname(self):
+		response = self.app.get('/search?fname=sangram&lname=''&cname=''')
+		people_search_count = (json.loads(response.data)[u'numResults'])
+		self.assertGreater(people_search_count, 0)
+	
+	def test_search_lname(self):
+		response = self.app.get('/search?fname=&lname=kapre&cname=')
+		people_search_count = (json.loads(response.data)[u'numResults'])
+		self.assertGreater(people_search_count, 0)
+
+	def test_search_fname_lname(self):
+		response = self.app.get('/search?fname=sangram&lname=kapre&cname=.')
+		people_search_count = (json.loads(response.data)[u'numResults'])
+		self.assertGreater(people_search_count, 0)
+		
+	# linkedin account
+
 	def test_add_account(self):
 		response = self.app.get('/add_account')
-		#print response.data
 		self.assertEqual(response.status_code, 200)
 
+	def test_add_account_url(self):
+		response = self.app.get('/add_account')
+		response_prefix = 'https://api.linkedin.com/uas/oauth/authenticate?'
+		self.assertTrue(response_prefix in response.data)
+
+	# login
 	def test_login(self):
 		response = self.app.get('/login')
 		#print response.data
 		self.assertEqual(response.status_code, 200)
 
+	# return_leads
 	def test_return_leads(self):
 		response = self.app.get('/return_leads')
 		#print response.data
@@ -44,9 +69,14 @@ class LeadsTestCase(unittest.TestCase):
 
 	def test_return_leads_count(self):
 		response = self.app.get('/return_leads')
-		print len(json.loads(response.data))
-		self.
-		
+		number_of_leads = len(json.loads(response.data))
+		self.assertGreater(number_of_leads, 0)
+
+	# fetch_profile
+	def test_fetch_profile(self):
+		response = self.app.get('/fetch_profile?lead_number=1&profile_id=')
+		#print response.data
+		self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
