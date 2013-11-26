b983b089428044803090a3d4205906f2afb9afff
Sangram Kapre <sangram@sangram-Ubuntu12.04>
2013-11-26 10:26:36 +0530

    changes to email contents

--- app/app.py
+++ app/app.py
@@ -90,8 +90,8 @@ def invite():
 	
 	# send invitation email
 	u = Util()
-	subject = "invitation to Leads' In"
-	text = "Click " + add_account_url + " to add account to Leads' In. Thank you."
+	subject = "Invitation to join Leads' In"
+	text = "Hello, \nPlease click " + add_account_url + " to add yourself to the list of leads in \"Leads' In\". \nThank you."
 	u.send_invitation_email(email, subject, text)
 	
 	return jsonify({'response':True, 'email':email})
